62dcb5d14e26fffbedb91ab871400a519627e9d2
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-09 23:47:58 -1100

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        
+      </Router>
+    </main>
   );
 };
 
