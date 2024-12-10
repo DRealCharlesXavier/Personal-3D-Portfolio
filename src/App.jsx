949e721f5d53ef3009c491843b9bf390cc18a709
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-09 23:54:48 -1100

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
       <Router>
-        
+        <Navbar />
+        <Routes>
+          <Route path='/' element={'Home'} />
+          <Route path='/about' element={'About'} />
+          <Route path='/projects' element={'Projects'} />
+          <Route path='/Contact' element={'Contact'} />
+        </Routes>
       </Router>
     </main>
   );
