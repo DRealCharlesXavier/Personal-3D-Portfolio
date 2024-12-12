c990912853012a4f2a0fb71a3db82db2f735ef63
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-11 17:22:22 -1100

    4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 {
   /* <div className="absolute top-80 left-0 right-0 z-10 flex items-center justify-center ">
@@ -38,6 +39,7 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
 
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
